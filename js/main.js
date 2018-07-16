bb5d6fcebebcb2be8bbb7e77866e310748424d14
danieleve <daniel-eve1@hotmail.com>
2018-07-16 00:32:44 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-$(document).ready(function() {
+ $("#menuPrincipalfail").click(function(){
+      window.location.href = "index.html";
+    });
     
-    $("#imagenes > a:gt(0)").hide();
+  $("#menuPrincipalwin").click(function(){
+      window.location.href = "index.html";
+    });
 
+  $("#intentarOtra").click(function(){
+      window.location.href = "main.html?"+localStorage.getItem('juegoActual');
+    });
+
+ $("#imagenRegresa").ready(function(){
+     $('.mimagen').addClass('disable');
+    });
+
+ $("#cambio").click(function(){
+     window.location.href = "index.html";
+     //console.log("asd");
+});
+
+    $("#imagenes > a:gt(0)").hide();
     $("#siguiente").click(function(){
+
         $("#imagenes > a:first")
         .fadeOut(0)
         .next()
@@ -10,7 +29,7 @@ $(document).ready(function() {
         .end()
         .appendTo("#imagenes");
     });
-    
+
     $("#atras").click(function(){
         $("#imagenes > a:gt(0)")
         .fadeIn(0)
@@ -18,4 +37,41 @@ $(document).ready(function() {
         .fadeOut(0)
         .appendTo("#imagenes");
     });
+
+
+
+$("#slider1").click(function(){
+  var globalVariable=0
+  var queryString = "?"+ globalVariable;
+  window.location.href = "main.html" + queryString;
+try {
+    cargarArreglo();
+}
+catch(err) {
+    //console.log(err.message);
+}
+
+});
+$("#slider2").click(function(){
+
+  var globalVariable=1
+  var queryString = "?"+globalVariable;
+  window.location.href = "main.html" + queryString;
+try {
+    cargarArreglo();
+}
+catch(err) {
+    //console.log(err.message);
+}
+});
+$("#slider3").click(function(){
+  var globalVariable=2
+  var queryString = "?" + globalVariable;
+  window.location.href = "main.html" + queryString;
+try {
+    cargarArreglo();
+}
+catch(err) {
+    //console.log(err.message);
+}
 });
